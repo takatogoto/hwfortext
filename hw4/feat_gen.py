9b83d13cfa2341c0a190e1844cb53497237c52b6
takatogoto <42685217+takatogoto@users.noreply.github.com>
2019-11-28 15:02:27 -0800

    3

--- hw4/feat_gen.py
+++ hw4/feat_gen.py
@@ -31,12 +31,13 @@ def preprocess_corpus(train_sents):
     """
     if not 'x1_list' in globals():
         x1_list = lexicon_list(x1dir)
-    """
+    
     if not 'x2_list' in globals():
         x2_list = lexicon_list(x2dir)
     """"
     if not 'x3_list' in globals():
         x3_list = lexicon_list(x3dir)
+    """
     if not 'x4_list' in globals():
         x4_list = lexicon_list(x4dir)
     if not 'x5_list' in globals():
@@ -96,12 +97,13 @@ def token2features(sent, i, orig_len, add_neighs = True):
     """
     if word in x1_list:
         ftrs.append("IS_X1")
-    """
+    
     if word in x2_list:
         ftrs.append("IS_X2")
     """
     if word in x3_list:
         ftrs.append("IS_X3")
+    """
     if word in x4_list:
         ftrs.append("IS_X4")
     if word in x5_list:
